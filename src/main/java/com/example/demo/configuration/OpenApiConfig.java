a40e28d74da96081a9eed77b561554b932f5958e
Akhil <akhilpgvr@gmail.com>
2024-06-15 16:01:16 +0530

    added skeleton

---
+++ src/main/java/com/example/demo/configuration/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("My Spring Boot API")
+                        .description("Sample API for demonstrating OpenAPI integration")
+                        .version("v1.0")
+                        .contact(new Contact().name("Your Name").email("yourname@example.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
+
---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @PostMapping("/test")
+    public String setName(@RequestParam String name){
+        return name;
+    }
+}
